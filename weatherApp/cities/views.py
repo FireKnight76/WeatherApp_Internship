9bfd79435fae4a627a5c2bebfc2e789581aae4cf
FireKnight76 <fireknight593@gmail.com>
2025-12-13 13:09:51 +0100

    basic html page
    replaced the string of the view with a basic html page

--- weatherApp/cities/views.py
+++ weatherApp/cities/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def cities(request):
-    return HttpResponse("request")
+    template = loader.get_template('mainPage.html')
+    return HttpResponse(template.render())
