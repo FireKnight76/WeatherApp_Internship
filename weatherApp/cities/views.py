472c5626d915a54efb824cddeca13bd5213f3cd2
FireKnight76 <fireknight593@gmail.com>
2025-12-13 15:04:36 +0100

    request weather conditions in terminal
    With the code in the view the program can now ask and receive information about the requested cities

--- weatherApp/cities/views.py
+++ weatherApp/cities/views.py
@@ -1,13 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+import requests
 from .models import City
 
 # Create your views here.
 def cities(request):
-    myCities = City.objects.all().values()
+    url = 'http://api.weatherapi.com/v1/current.json?key=7773446b649e406e80d123250251312&q={}&aqi=no'
+    weather_info = []
+
+    myCities = City.objects.values('cityName')
+
+    for city in myCities:
+
+        
+        get_city_weather = requests.get(url.format(city)).json()
+        
+        weather_info.append(get_city_weather)
+    #     weather = {
+    #         'city': city,
+    #         'temp': get_city_weather['main']['temp']
+    #     }
+
+    for city in weather_info:
+        print(city)
+
     template = loader.get_template('mainPage.html')
     context = {
-        'myCities': myCities,
+        'weatherInfo': weather_info,
     }
     return HttpResponse(template.render(context, request))
+
+
+   # return render(request, 'mainPage.html')
