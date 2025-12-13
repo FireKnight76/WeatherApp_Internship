53c6b7f052f81bd1a080fb438d5aa4d787b00bfb
FireKnight76 <fireknight593@gmail.com>
2025-12-13 13:28:40 +0100

    expanded mainPage
    Main page now shows the contents of the database

--- weatherApp/cities/views.py
+++ weatherApp/cities/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import City
 
 # Create your views here.
 def cities(request):
+    myCities = City.objects.all().values()
     template = loader.get_template('mainPage.html')
-    return HttpResponse(template.render())
+    context = {
+        'myCities': myCities,
+    }
+    return HttpResponse(template.render(context, request))
