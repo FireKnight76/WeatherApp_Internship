2a6a1422824b74d3483ec967f5241ee865b82ae4
FireKnight76 <fireknight593@gmail.com>
2025-12-13 16:57:02 +0100

    Cities can be added
    finalized the form in the mainPage to now add cities to the list

--- weatherApp/cities/views.py
+++ weatherApp/cities/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 import requests
@@ -6,13 +6,21 @@ from .models import City
 
 # Create your views here.
 def cities(request):
+    #the link with my api key to access the weather api
     url = 'http://api.weatherapi.com/v1/current.json?key=7773446b649e406e80d123250251312&q={}&aqi=no'
     weather_info = []
+
+    if request.method == "POST":
+        city_name = request.POST.get("city")
+        if city_name:
+            City.objects.get_or_create(cityName=city_name)
+        return redirect("cities")
 
     myCities = City.objects.values_list('cityName',flat=True)
 
     for city in myCities:
-
+        
+        #requests the data from the api, and inserts the desired locations
         city_status = requests.get(url.format(city)).json()
 
         situation   = {
@@ -23,14 +31,9 @@ def cities(request):
 
         weather_info.append(situation)
 
-
-    # for city in weather_info:
-    #     print(city)
-
     template = loader.get_template('mainPage.html')
     context = {
         'weather_info': weather_info,
     }
 
     return HttpResponse(template.render(context, request))
-    #return render(request, 'mainPage.html', context)
