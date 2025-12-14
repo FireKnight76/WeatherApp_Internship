fe1a843502871ba975151697bdcf1bd061b31344
FireKnight76 <fireknight593@gmail.com>
2025-12-14 11:52:10 +0100

    added the function to check earlier dates
    there is now a function that allows one to see the weather conditions of earlier dates, I might add some more details but overall the assignment is done

--- weatherApp/cities/views.py
+++ weatherApp/cities/views.py
@@ -9,9 +9,8 @@ from datetime import date
 def cities(request):
     #the link with my api key to access the weather api
     url = 'http://api.weatherapi.com/v1/current.json?key=7773446b649e406e80d123250251312&q={}'
-    now = str(date.today())
     city = "Almere"
-    
+
 
     #method for adding cities
     if request.method == "POST" and "add_city" in request.POST:
@@ -20,22 +19,40 @@ def cities(request):
             City.objects.get_or_create(cityName=city_name)
         return redirect("cities")
     
+    
     if request.method == "POST" and "view_city" in request.POST:
         city = request.POST.get('city_list')
-        # return redirect('cities')
+
 
     myCities = City.objects.values_list('cityName',flat=True)
 
-
+    
+    if request.method == "POST" and "view_date" in request.POST:
+        weather_date = str(request.POST.get('city_date'))
+        url = 'http://api.weatherapi.com/v1/history.json?key=7773446b649e406e80d123250251312&q={}&dt={}'
         
-    #requests the data from the api, and inserts the desired locations
-    city_status = requests.get(url.format(city)).json()
+        #requests the data from the api, and inserts the desired locations
+        city_status = requests.get(url.format(city, weather_date)).json()
 
-    weather_info   = {
-        'city': city,
-        'time': city_status['location']['localtime'],
-        'temp_c': city_status['current']['temp_c'],
-    }
+        weather_info   = {
+            'city': city,
+            'date': city_status['forecast']['forecastday'][0]['date'],
+            'maxtemp_c': city_status['forecast']['forecastday'][0]['day']['maxtemp_c'],
+            'mintemp_c': city_status['forecast']['forecastday'][0]['day']['mintemp_c'],
+            'mode': 'history'
+        }
+    else:
+        city_status = requests.get(url.format(city)).json()
+        
+        weather_info   = {
+            'city': city,
+            'time': city_status['location']['localtime'],
+            'temp_c': city_status['current']['temp_c'],
+            'mode': 'current'
+        }
+
+
+    
 
 
     template = loader.get_template('mainPage.html')
@@ -44,6 +61,6 @@ def cities(request):
         'weather_info': weather_info,
     }
 
-    print(weather_info)
+    print(city_status)
 
     return HttpResponse(template.render(context, request))
