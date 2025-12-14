67162e370ccb730240a9127d9f80a25dad5200ff
FireKnight76 <fireknight593@gmail.com>
2025-12-14 13:16:14 +0100

    code cleanup and readme
    worked to improve the consistency of the code and explained my work methods in the readme
    also added more comments for clarification regarding the code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # WeatherApp_Internship
 
+In the process of making this REST api that can translate the data from a weather api and show it on a website, I made some mistakes.
+
+I was excited to start learning a new framework and working on something I have never done before and in my excitement I accidentally showed all the cities and their selected weather data.
+
+I managed to change it back to the desired result the next day but it was still a bummer that I spent a bunch of time going down the wrong road. However it was a valuable experience and still kind of fun to figure that stuff out, even if it wasn't necessary.
+
+I followed the basic structure I have been learning on [w3schools](https://www.w3schools.com/django/index.php) since that seemed to work. I decided to focus on the functionality instead of the exterior of the website and tried to stick to the coding conventions as much as I could.
+
+It is still kind of messy since I have not done any real project for a while, only research due to some subjects that did not require any coding. I especially have not done anything with html for about 2 years now which I could definitely notice.
+
+For the technical side I decided to save the cities in a list that drops down in favor of space and to make the website look a bit neater. I also saved them in a list since that seemed like a simple solution for viewing and finding the desired city. From there the software can request the provided name to input it into the api url to request the data.
+
+The date was a bit trickier, but I made it work by using if statements. This statement checks if the user selected a specific date and if they did the software will pick the url necessary for viewing the historical data.
+
+I originally wanted to place the Weather status in the middle of the page but did not manage to because I spent all my time fixing my mistakes.
--- weatherApp/cities/views.py
+++ weatherApp/cities/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 from django.template import loader
 import requests
 from .models import City
-from datetime import date
 
 # Create your views here.
 def cities(request):
@@ -18,23 +17,27 @@ def cities(request):
             City.objects.get_or_create(cityName=city_name)
         return redirect("cities")
     
-
+    #checks the current value of the select tag on the mainPage
     if request.method == "POST":
         selected_city = request.POST.get('city_list')
         if selected_city:
             city = selected_city
 
 
-    myCities = City.objects.values_list('cityName',flat=True)
+    #list that checks which cities have been added by the user
+    my_cities = City.objects.values_list('cityName',flat=True)
 
-    
+    #checks if the user requests a specific date
     if request.method == "POST" and "view_date" in request.POST:
+        #saves the selected date as a string to put into the url
         weather_date = str(request.POST.get('city_date'))
+        #the url for historical data that overwrites the original url to get the desired data
         url = 'http://api.weatherapi.com/v1/history.json?key=7773446b649e406e80d123250251312&q={}&dt={}'
         
         #requests the data from the api, and inserts the desired locations
         city_status = requests.get(url.format(city, weather_date)).json()
 
+        #the created object to fit with the returned data regarding the city's past weather conditions
         weather_info   = {
             'city': city_status['location']['name'],
             'date': city_status['forecast']['forecastday'][0]['date'],
@@ -45,6 +48,7 @@ def cities(request):
     else:
         city_status = requests.get(url.format(city)).json()
         
+        #the created object to fit with the returned data regarding the city's current weather conditions
         weather_info   = {
             'city': city_status['location']['name'],
             'time': city_status['location']['localtime'],
@@ -52,16 +56,11 @@ def cities(request):
             'mode': 'current'
         }
 
-
-    
-
-
     template = loader.get_template('mainPage.html')
+    #saves the list and object for the mainPage to use
     context = {
-        'myCities': myCities,
+        'my_cities': my_cities,
         'weather_info': weather_info,
     }
-
-    print(city_status)
 
     return HttpResponse(template.render(context, request))
