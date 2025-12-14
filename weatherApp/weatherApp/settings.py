d75d93cc7870032cd10fab5694eaf1dda958a022
FireKnight76 <fireknight593@gmail.com>
2025-12-14 12:33:16 +0100

    changed setting with env

--- weatherApp/weatherApp/settings.py
+++ weatherApp/weatherApp/settings.py
@@ -11,19 +11,23 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9308j#xopl61$mg76ooo!mjxi3r-u$yep$4jicy=+dylvq=uhx'
+SECRET_KEY = environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = []
 
